105b4f324f1dbd6cee552786bf8388fa57072288
ankurparihar <pariharankur101@gmail.com>
2020-08-08 09:58:01 +0530

    script with page-style

--- script.js
+++ script.js
@@ -1,60 +1,8 @@
 const timetable__data = {
 	navID: 'nav-projects',
 	page_loc_text: 'Live Demo',
-	template: `
-	<div id="demo_page" class="demo-page page">
-		<div class="demo_page-head flex">
-			<div class="demo_page-back btn" title="Back to all projects">
-				<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z"/></svg>
-			</div>
-			<div class="demo_page-title">Time-Table Generator</div>
-			<div class="demo_page-info btn" title="Info">
-				<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"/></svg>
-			</div>
-		</div>
-		<div class="demo_page-body">
-			<div class="demo_page-cont">
-				<div id="timetable_holder">
-					<h2>Department of Computer Science &amp; Engineering</h2>
-					<h3>Indian Institute of Technology Roorkee</h3>
-					<h3>Autumn 2019 - 2020</h3>
-					<h4>Time Table of B.Tech IV year</h4>
-					<table id="timetable" class="noselect"></table>
-				</div>
-				<div id="timetable_options">
-					<p id="desc">No cell selected</p>
-					<div id="options" class="flex wrap"></div>
-					<div id="copy_paste">
-						<button class="btn" aria-label="copy cells" id="button_copy">Copy cells</button>
-						<button class="btn" aria-label="paste cells" id="button_paste">Paste cells</button>
-						<button class="btn" aria-label="clear cells" id="button_clear">Clear cells</button>
-					</div>
-					<div id="cell_select_flip">
-						<button class="btn" aria-label="enable multi select" id="button_flip_select">Enable multi-select</button>
-						<button class="btn" aria-label="deselect all cells" id="button_deselect">Deselect all cells</button>
-					</div>
-				</div>
-				<div id="timetable_manual">
-					<p id="title" class="highlight">Instructions and Features</p>
-					<ul>
-						<li>If you're on mobile device, rotate to landscape</li>
-						<li>Select single cells by clicking</li>
-						<li>If selected cells are of same type (<span class="highlight">data</span> or <span class="highlight">header</span>), then options are displayed</li>
-						<li>Select/Deselect multiple cells by <code style="color:#cfbf2f">button</code> or by holding <code>ctrl-key</code></li>
-						<li>Deselect all cells by <code style="color:#cf7814">button</code></li>
-						<li>Copy cells by <code style="color:#4caf50">button</code> or <code>ctrl+c</code></li>
-						<li>Paste cells by <code style="color:#4c9baf">button</code> or <code>ctrl+v</code></li>
-						<li>Clear cells by <code style="color:#cf3a2f">button</code> or <code>delete</code></li>
-						<li>Copy, Paste and Clear works with <span class="highlight">data</span> cells only</li>
-						<li>Merge cells row-wise or column-wise, called <span class="highlight">spanning</span> (from script only)</li>
-						<li>Table is configured using <span class="highlight"><code>timetable__data</code></span> in <a class="highlight" href="https://github.com/ankurparihar/Time-Table/blob/github.io-dev/script.js">script.js</a></li>
-						<li>Right now screenshot is the only way to save table</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	`,
+	template: `<div id="demo_page" class="demo-page page"><div class="demo_page-head flex"><div class="demo_page-back btn" title="Back to all projects"><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z"/></svg></div><div class="demo_page-title">Time-Table Generator</div><div class="demo_page-info btn" title="Info"><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"/></svg></div></div><div class="demo_page-body"><div class="demo_page-cont"><div id="timetable_holder"><h2>Department of Computer Science &amp; Engineering</h2><h3>Indian Institute of Technology Roorkee</h3><h3>Autumn 2019 - 2020</h3><h4>Time Table of B.Tech IV year</h4><table id="timetable" class="noselect"></table></div><div id="timetable_options"><p id="desc">No cell selected</p><div id="options" class="flex wrap"></div><div id="copy_paste"><button class="btn" aria-label="copy cells" id="button_copy">Copy cells</button><button class="btn" aria-label="paste cells" id="button_paste">Paste cells</button><button class="btn" aria-label="clear cells" id="button_clear">Clear cells</button></div><div id="cell_select_flip"><button class="btn" aria-label="enable multi select" id="button_flip_select">Enable multi-select</button><button class="btn" aria-label="deselect all cells" id="button_deselect">Deselect all cells</button></div></div><div id="timetable_manual"><p id="title" class="highlight">Instructions and Features</p><ul><li>If you're on mobile device, rotate to landscape</li><li>Select single cells by clicking</li><li>If selected cells are of same type (<span class="highlight">data</span> or <span class="highlight">header</span>), then options are displayed</li><li>Select/Deselect multiple cells by <code style="color:#cfbf2f">button</code> or by holding <code>ctrl-key</code></li><li>Deselect all cells by <code style="color:#cf7814">button</code></li><li>Copy cells by <code style="color:#4caf50">button</code> or <code>ctrl+c</code></li><li>Paste cells by <code style="color:#4c9baf">button</code> or <code>ctrl+v</code></li><li>Clear cells by <code style="color:#cf3a2f">button</code> or <code>delete</code></li><li>Copy, Paste and Clear works with <span class="highlight">data</span> cells only</li><li>Merge cells row-wise or column-wise, called <span class="highlight">spanning</span> (from script only)</li><li>Table is configured using <span class="highlight"><code>timetable__data</code></span> in <a class="highlight" href="https://github.com/ankurparihar/Time-Table/blob/github.io-dev/script.js">script.js</a></li><li>Right now screenshot is the only way to save table</li></ul></div></div></div></div>`,
+	style: `@media (max-width:499px){#demo_page{max-width:100vw;overflow:hidden}}#demo_page .demo_page-head{padding-top:16px;padding-bottom:16px;width:100%;border-bottom:3px double;height:80px}#demo_page .demo_page-back,#demo_page .demo_page-info{background-color:transparent;box-shadow:none}#demo_page .demo_page-back svg,#demo_page .demo_page-info svg{cursor:pointer}#demo_page .demo_page-back svg:hover,#demo_page .demo_page-info svg:hover{color:#f3c669}#demo_page button{outline:0}#demo_page .demo_page-title{width:100%;font-size:28px;text-align:center;margin:auto}@media (max-width:600px){#demo_page .demo_page-title{font-size:4.5vw}}.demo_page-body .demo_page-cont{border-bottom:3px double}#timetable_holder table{margin-left:auto;margin-right:auto;font-family:'Times New Roman'}#timetable_holder h2{text-align:center;font-size:28px;margin:20px auto 10px auto}#timetable_holder h3{text-align:center;font-size:24px;font-weight:400;margin:10px}#timetable_holder h4{text-align:center;font-size:20px;margin:10px}#timetable,#timetable tr td,#timetable tr th{border:1px solid #fff;border-collapse:collapse}#timetable tr td,#timetable tr th{text-align:center;padding:5px;width:12.5vw;max-width:150px;cursor:cell;font-size:1.5vw}@media (min-width:960px){#timetable tr td,#timetable tr th{width:150px;min-width:150px;font-size:14px}}#timetable tr td p{margin:2px}#timetable tr td.active,#timetable tr th.active{background-color:#abbaab}#timetable tr .c0,#timetable tr .r0{background-color:var(--theme-color,#f3c669);color:#343434}#timetable_manual,#timetable_options{margin:20px auto;border:3px double;max-width:1200px;text-align:center;font-family:"Segoe UI",Arial,sans-serif;font-weight:400}#title{margin-top:5px;text-align:center;font-size:20px}#timetable_manual ul{list-style-type:disc;padding-inline-start:40px;text-align:left;margin:35px}@media (max-width:599px){#timetable_manual ul{list-style-type:disc;padding-inline-start:10px;text-align:left;margin:20px}}#options{display:flex;justify-content:center}#desc{font-size:20px;margin:15px auto 5px auto;justify-content:center}#options input::selection{background-color:#f3c669!important}#options select{text-align:center;text-align-last:center;display:inline-block;position:relative;overflow:hidden;margin:10px;margin-top:0;height:28px;width:150px;background:#eee;color:#343434;border:0;outline:0;-webkit-box-shadow:0 1px 1px rgba(0,0,0,.08);box-shadow:0 1px 1px rgba(0,0,0,.08);appearance:menulist-button;-moz-appearance:menulist-button;-webkit-appearance:menulist}#options input{text-align:center;position:relative;margin:10px;height:28px;width:150px;background:#eee;color:#343434;border:0;outline:0;-webkit-box-shadow:0 1px 1px rgba(0,0,0,.08);box-shadow:0 1px 1px rgba(0,0,0,.08)}#cell_select_flip button,#copy_paste button{border:2px transparent;color:#fff;width:150px;padding:5px 22px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;border-radius:4px;outline:0;cursor:pointer;margin-top:10px}#cell_select_flip button{margin-top:20px;margin-bottom:20px;width:200px}#button_copy{background-color:#4caf50}#button_copy:hover{background-color:#eee;border:2px solid #4caf50;color:#000}#button_paste{background-color:#4c9baf}#button_paste:hover{background-color:#eee;border:2px solid #4c9baf;color:#000}#button_clear{background-color:#cf3a2f}#button_clear:hover{background-color:#eee;border:2px solid #cf3a2f;color:#000}#button_flip_select{background-color:#cfbf2f}#button_flip_select:hover{background-color:#eee;border:2px solid #cfbf2f;color:#000}#button_deselect{background-color:#cf7814}#button_deselect:hover{background-color:#eee;border:2px solid #cf7814;color:#000}`,
 	apply: (root, URLInfo) => {
 		if (root === undefined) {
 			console.warn('Error: contentRoot not specified')
